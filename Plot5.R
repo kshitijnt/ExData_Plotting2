2bf26a5e81a91d48cced53b53ed42ab40eba0e77
kshitij Tumbde <kshitijnt@yahoo.com>
2015-03-22 15:34:05 -0700

    Changes

--- Plot5.R
+++ Plot5.R
@@ -2,8 +2,9 @@ setwd("~/Documents/Data Science/PollutantData_Project")
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 NEIBaltimore <- NEI [NEI$fips =="24510",]
-BaltimoreAgg <- aggregate(NEIBaltimore$Emissions ~ NEIBaltimore$year,FUN=sum)
-colnames(BaltimoreAgg) <- c("Year", "Emissions")
+BaltMotor <- NEIBaltimore[NEIBaltimore$type == "ON-ROAD",]
+BaltMotorAgg <- aggregate(BaltMotor$Emissions ~ BaltMotor$year,FUN=sum)
+colnames(BaltMotorAgg) <- c("Year", "Emissions")
 png("plot5.png", height=480,width=480)
-plot(BaltimoreAgg$Year,BaltimoreAgg$Emissions,type="l",ylab="Baltimore PM2.5 Emisisons",xlab="Year",col="blue" ,main="Baltimore PM2.5 Emissions")
+plot(BaltMotorAgg$Year,BaltMotorAgg$Emissions,type="l",ylab="PM2.5 Emisisons",xlab="Year",col="green" ,main="Baltimore City Motor Vehicle Emissions by Year")
 dev.off()
